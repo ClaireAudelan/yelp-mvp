db465d3512ca8676e7a8bf0bdd94efcb603cfe20
Claire Lo <caudelan@gmail.com>
2022-01-18 20:43:02 +0000

    create validations, seeds, routes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, :rating, presence: true
+  validates :content, :rating, :restaurant_id, presence: true
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5], message: 'The rating must be a number between 0 and 5.' }
-
+  validates :rating, numericality: { only_integer: true }
 end
